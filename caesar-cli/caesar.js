796338f03b694bd4ea2e30046d71ab7052680d3d
vof <vof1111@gmail.com>
2020-10-07 02:53:59 +0300

    feat: add cipher logic

--- caesar-cli/caesar.js
+++ caesar-cli/caesar.js
@@ -14,18 +14,23 @@ let inFile, outFile;
 if(opts.input)
     inFile = fs.createReadStream(opts.input, {encoding: 'utf8'});
 if(opts.output)
-    outFile = fs.createWriteStream(opts.output, {flags: 'r+', encoding: 'utf8'});
+    outFile = fs.createWriteStream(opts.output, {
+        flags: fs.constants.O_WRONLY|fs.constants.O_APPEND,
+        encoding: 'utf8'
+    });
 
 pipeline(
     inFile || process.stdin,
     transformer( opts.action, opts.shift ),
     outFile || process.stdout,
     (err) => {
-        process.exitCode = 1;
-        if(err.code == 'ENOENT')
-            console.error(`file error: ${err.path}`);
-        else
-            console.error(err)
+        if(err) {
+            process.exitCode = 1;
+            if(err.code == 'ENOENT')
+                console.error(`file does not exist: ${err.path}`);
+            else
+                console.error(err.toString());
+        }
     }
 )
 
